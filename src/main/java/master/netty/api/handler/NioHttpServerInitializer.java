942beef29b42a4cad66f87220d3d67c13f9ae9d8
jason.liuhongming <jason.liuhongming@gmail.com>
2015-08-29 12:06:20 +0800

    修改： 提交修改心医咨询室资料

--- src/main/java/master/netty/api/handler/NioHttpServerInitializer.java
+++ src/main/java/master/netty/api/handler/NioHttpServerInitializer.java
@@ -1,16 +1,26 @@
 package master.netty.api.handler;
 
 import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.SocketChannel;
+import io.netty.handler.codec.http.HttpObjectAggregator;
 
 public class NioHttpServerInitializer extends ChannelInitializer<SocketChannel> {
 
+    private boolean enableSSL;
+
     public NioHttpServerInitializer(boolean enableSSL) {
+        this.enableSSL = enableSSL;
     }
 
     @Override
     protected void initChannel(SocketChannel ch) throws Exception {
 
+        ChannelPipeline pipeline = ch.pipeline();
+        pipeline.addLast("handler", new NioHttpServerHandler());
+        pipeline.addLast("aggregator", new HttpObjectAggregator(65535));
+        pipeline.addLast("encoder", null);
+        pipeline.addLast("decoder", null);
 
     }
 
