9d8b68f12bee107b033201f614eaff0d1b07ee16
jason.liuhongming <jason.liuhongming@gmail.com>
2015-10-22 13:11:26 +0800

    Source code

---
+++ src/main/java/master/netty/api/codec/MesageEncoder.java
@@ -0,0 +1,8 @@
+package master.netty.api.codec;
+
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+
+public class MesageEncoder extends ChannelOutboundHandlerAdapter {
+
+
+}
---
+++ src/main/java/master/netty/api/codec/MessageDecoder.java
@@ -0,0 +1,7 @@
+package master.netty.api.codec;
+
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class MessageDecoder extends ChannelInboundHandlerAdapter {
+
+}
